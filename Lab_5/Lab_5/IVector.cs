6a883ef85ce6433224d15f958566e7e748a9edae
kannar04 <manhle240406@gmail.com>
2026-01-31 22:23:39 +0700

    Delete Lab_5 directory

--- Lab_5/Lab_5/IVector.cs
+++
@@ -1,7 +0,0 @@
-public interface IVector : ICloneable, IComparable
-{
-    float Module();
-    IVector Add(IVector other);
-    IVector Subtract(IVector other);
-    float DotProduct(IVector other);
-}
